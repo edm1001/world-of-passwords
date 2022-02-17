cb05753ffa92226c139e2ea5de143f04a0e25280
Edmer Valencia <edmer_franciz@yahoo.com>
2022-02-17 02:49:16 -0600

    Added weather API

--- Project-01-DogDash/Assets/JS/script.js
+++ Project-01-DogDash/Assets/JS/script.js
@@ -4,7 +4,7 @@ var userNameInput = document.querySelector("#user-name");
 var userEmailInput = document.querySelector("#user-email");
 var userAddressInput = document.querySelector("#user-address");
 var userInfoInput = document.querySelector("#user-info");
-var userSignUp= document.querySelector("#user-submit")
+var userSignUp= document.querySelector("#user-submit");
 
 
 userSignUp.addEventListener("click", function(event) {
@@ -45,4 +45,25 @@ var walker= {
 localStorage.setItem("walker",JSON.stringify(walker));
 });
 
+//apiKey5687dedd3daac998d60647cb89f0066d
+//Weather Api
+var button=document.querySelector(".button");
+var weatherInput=document.querySelector(".weatherInput")
+var cityName=document.querySelector(".name");
+var info=document.querySelector(".info");
+var temp=document.querySelector(".temp");
 
+button.addEventListener("click", function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+weatherInput.value+'&appid=5687dedd3daac998d60647cb89f0066d')
+    .then(response => response.json())
+    .then(data => {
+        var townName = date['name'];
+        var cityTemp =data['main']['temp'];
+        var weathInfo = data['weather'][0]['description'];
+
+        cityName.innerHTML = townName;
+        temp.innerHTML=cityTemp;
+        info.innerHTML= weathInfo;
+    })
+    .catch(error= alert("Cannot locate this location"))
+})
